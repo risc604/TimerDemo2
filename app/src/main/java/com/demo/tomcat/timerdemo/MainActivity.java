01d47df0fdd138ce6a4cedb123900816c3cf5a0b
tomcat <tomcat.cheng@microlife.com.tw>
2018-03-16 17:02:30 +0800

    init code

---
+++ app/src/main/java/com/demo/tomcat/timerdemo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.demo.tomcat.timerdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+// http://boywhy.blogspot.tw/2015/04/javaandroid-timer.html
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
